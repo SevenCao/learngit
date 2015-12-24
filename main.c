2cfa3fa4719d16187c44b4545f482bac4654e93a
_sevenCao <406238940@qq.com>
2015-12-24 16:48:07 +0800

    once

---
+++ main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  eg2.1
+//
+//  Created by luhongyu on 15/8/28.
+//  Copyright (c) 2015年 Hogan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    
+    int j = 1;
+    for (int i = 2; i < 6; i ++)
+    {
+        j = j*i;
+    }
+    
+    printf("%d\n",j);
+    
+    return 0;
+}
